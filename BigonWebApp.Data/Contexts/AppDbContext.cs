39489770ccf93abe70429a1063ae200f4530f8a1
aliyevvalii66 <aliyevvalii66@gmail.com>
2024-06-04 02:50:13 +0400

    added

---
+++ BigonWebApp.Data/Contexts/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BigonWebApp.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigonWebApp.Data.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions opt):base(opt) { }
+        public DbSet<Color> Colors { get; set; }
+
+
+
+
+
+    }
+}
---
+++ BigonWebApp.MVC/Areas/Admin/Controllers/ColorsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BigonWebApp.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ColorsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
